6592a39d850b6a9a6c69636a8179c99b1fe9453c
aminaasif20 <aminaasif245@gmail.com>
2026-02-23 19:28:33 +0500

    commit

--- react/vite-project/src/components/doDo/TodoApp.jsx
+++ react/vite-project/src/components/doDo/TodoApp.jsx
@@ -1,9 +1,22 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const TodoApp = () => {
     const [todos,setTodo]=useState([]);
     const [input,setInput]=useState("");
+    const [editinput,setEditInput]=useState("");
     const [editindex,setEditindex]=useState(null);
+    const [date,setDate]=useState(new Date());
+    
+    useEffect(()=>{
+        const timer=setInterval(() => {
+            setDate(new Date());
+        }, 1000);
+        return()=>{
+            clearInterval(timer);
+        }
+    },[]);
+
+
 
     const handleInputChange=(value)=>{
         setInput(value);
@@ -37,11 +50,14 @@ const TodoApp = () => {
         const newToDo=todos.filter((_,i)=>i!==index);
         setTodo(newToDo);
     }
+    
     const handleEdit=(index)=>{
         setEditindex(index);
-        setInput(todos[index].text);
+        setEditInput(todos[index].text);
+        // handleTask();
 
     }
+
     const handleIncrement=(index)=>{
         setTodo(
             todos.map((item,i)=>
@@ -71,10 +87,22 @@ const TodoApp = () => {
             }))
         )
     }
+    const handleSave = () => {
+        const trimmedText = editinput.trim();
+        if (!trimmedText) {
+            alert("Task cannot be empty");
+            return;
+        }
+        const newTodos = [...todos];
+        newTodos[editindex].text = trimmedText; // update text of specific item
+        setTodo(newTodos); // update state
+        setEditindex(null); // exit edit mode
+        setEditInput(""); // clear edit input
+    };
 
   return (
     <>
-    <div className='mt-1 bg-gray-200'>
+    <div className='mt-1'>
         <label className='p-2 m-2 mr-20 '> Enter Task </label>
 
         <input 
@@ -88,8 +116,15 @@ const TodoApp = () => {
         <button
         onClick={handleTask}
         className='bg-blue-500 p-2 mr-14 m-2 text-white rounded hover:bg-blue-700'    
-        >{editindex!==null ? "save Task":"Add Task"}</button>
+        >Add Task</button>
         <button onClick={handleReset} className='bg-red-600 font-medium p-1 px-2 m-2 rounded text-white hover:bg-red-700'>Reset Number of Persons</button>
+      <input
+            className='bg-red-600 text-3xl p-4 ml-20 rounded text-taupe-50 mt-2'
+            type="text"
+            value={`${date.toLocaleDateString()} ${date.toLocaleTimeString()}`}
+            readOnly
+        />
+
         <section>
             <ul>
                 {
@@ -102,8 +137,34 @@ const TodoApp = () => {
                             
                             <span  className={item.value===0 ? "bg-yellow-500 m-2 p-1.5 mr-3":"bg-blue-500 m-2 p-1.5 mr-3"}>{item.value===0? "person: 0":`person : ${item.value}`}</span>
 
-                            <span className='bg-blue-400 text-white px-2 p-1 rounded'>{item.text}</span>
-                            <button className='bg-green-600 font-medium p-1 px-2 m-2 rounded text-white hover:bg-green-700' onClick={()=>{handleEdit(i)}}>{editindex===i ? "Save":"Edit"}</button>
+                            {editindex===i?(
+                                <>
+                                <input type="text"
+                                className='border'
+                                value={editinput}
+                                onChange={(e)=>setEditInput(e.target.value)}
+                                />
+                                <button className='bg-green-600 font-medium p-1 px-2 m-2 rounded text-white' onClick={ handleSave}>save</button>
+                                </>
+
+                            ):(
+                                <>
+                                <span className='bg-blue-400 text-white px-2 p-1 rounded'>
+                                {item.text}
+                                </span>
+
+                                <button
+                                className='bg-green-600 font-medium p-1 px-2 m-2 rounded text-white'
+                                onClick={() => handleEdit(i)}
+                                >
+                                Edit
+                                </button>
+                                
+                                </>
+                            )
+                            }
+                            
+
                             <button className='bg-red-600 font-medium p-1 px-2 m-2 rounded text-white hover:bg-red-700' onClick={()=>{handleDelete(i)}}>Delete</button>
                         </li>
                     ))

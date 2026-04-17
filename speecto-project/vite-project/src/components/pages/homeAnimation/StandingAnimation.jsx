fbbe7da3fe68fe463b20c272db3a617eba9b39b8
aminaasif20 <aminaasif245@gmail.com>
2026-04-17 23:17:11 +0500

    add StandingAnimation component with floating platform and dashboard assets

--- speecto-project/vite-project/src/components/pages/homeAnimation/StandingAnimation.jsx
+++ speecto-project/vite-project/src/components/pages/homeAnimation/StandingAnimation.jsx
@@ -3,7 +3,9 @@ import "./StandingAnimation.css";
 
 import manImg from "../../../assets/asset/man.webp";
 import baseImg from "../../../assets/asset/steps.webp";
+import baseImg1 from "../../../assets/asset/stepsBottom.webp";
 import dashboard1 from "../../../assets/asset/psRound1.webp";
+import dashboard2 from "../../../assets/asset/psRound2.webp";
 // import dashboard2 from "../../../assets/asset/productionWave.webp";
 // import dashboard3 from "../../../assets/asset/psRound2.webp";
 
@@ -15,18 +17,24 @@ const StandingAnimation = () => {
         <div className="platform-base">
           <img src={baseImg} alt="Base Platform" className="platform-img" />
         </div>
+        <div className="platform-base-step">
+          <img src={baseImg1} alt="Base Platform" className="platform-img" />
+        </div>
 
         {/* Floating/Orbiting Dashboards */}
         <div className="orbiting-dashboards">
           <div className="dashboard orbit-1">
             <img src={dashboard1} alt="Chart 1" />
           </div>
-          {/* <div className="dashboard orbit-2">
-            <img src={dashboard3} alt="Chart 2" />
-          </div>
-          <div className="dashboard orbit-3">
-            <img src={dashboard3} alt="Chart 3" />
+
+          {/* <div className="dashboard orbit-3">
+            <img src={dashboard2} alt="Chart 3" />
           </div> */}
+        </div>
+        <div className="orbiting-dashboards-base">
+          <div className="dashboard orbit-2">
+            <img src={dashboard2} alt="Chart 2" />
+          </div>
         </div>
 
         {/* Standing Man in Center */}
